488f0a1825454e3574d97996757b1bd4ef263a3c
Jaemin Lee <oyatrij@gmail.com>
2025-04-02 23:37:43 +0900

    feat: 당근마켓 클론 코딩

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:radish_app/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-      ),
-    );
+    return SplashScreen();
   }
 }
+
